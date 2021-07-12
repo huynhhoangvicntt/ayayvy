2305cada20fd46873b450a3aef95d3d45c0581b5
huynhhoangvicntt <hoangvicntt2k@gmail.com>
2021-07-12 14:03:27 +0700

    first commit

---
+++ app/src/main/java/com/example/calculatorex/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.calculatorex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+    private String input, answer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.tv);
+    }
+
+    public void ButtonClick(View view) {
+        Button button = (Button) view;
+        String data = button.getText().toString();
+
+        switch (data) {
+            case "CE":
+                if (input.length() > 0) {
+                    String newText = input.substring(0, input.length() - 1);
+                    input = newText;
+                }
+                break;
+            case "=":
+                Solve();
+                answer = input;
+                break;
+            default:
+                if (input == null) {
+                    input = "";
+                }
+                if (data.equals("+") || data.equals("-") || data.equals("÷")) {
+                    Solve();
+                }
+                input += data;
+        }
+        tv.setText(input);
+    }
+
+    private void Solve() {
+        if (input.split("\\×").length == 2) {
+            String number[] = input.split("\\×");
+            try {
+                double mul = Double.parseDouble(number[0]) * Double.parseDouble(number[1]);
+                input = mul + "";
+            } catch (Exception e) {
+
+            }
+        }
+        if (input.split("\\÷").length == 2) {
+            String number[] = input.split("\\÷");
+            try {
+                double div = Double.parseDouble(number[0]) / Double.parseDouble(number[1]);
+                input = div + "";
+            } catch (Exception e) {
+
+            }
+        }
+        if (input.split("\\-").length == 2) {
+            String number[] = input.split("\\-");
+            try {
+                double sub = Double.parseDouble(number[0]) - Double.parseDouble(number[1]);
+                input = sub + "";
+            } catch (Exception e) {
+
+            }
+        }
+        if (input.split("\\+").length == 2) {
+            String number[] = input.split("\\+");
+            try {
+                double sum = Double.parseDouble(number[0]) + Double.parseDouble(number[1]);
+                input = sum + "";
+            } catch (Exception e) {
+
+            }
+        }
+        String n[] = input.split( "\\.");
+        if (n.length > 1){
+            if (n[1].equals("0")){
+                input = n[0];
+            }
+        }
+    }
+}
